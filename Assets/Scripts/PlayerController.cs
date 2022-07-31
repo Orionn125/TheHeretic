71aeb2b895f62f0954e1f9c7d5daf4f5e30e85ac
Luka Filajdic <Luka.fila125@gmail.com>
2022-07-31 10:19:06 +0200

    Init
    Init push

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private Rigidbody2D rb;
+    private Animator anim;
+
+    private Vector2 moveVelocity;
+    public float speed;
+    private bool facingRight = true;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        var horizontal = Input.GetAxisRaw("Horizontal");
+        var vertical = Input.GetAxisRaw("Vertical");
+
+        Vector2 moveInput = new Vector2(horizontal, vertical);
+        moveVelocity = moveInput * speed;
+
+        if (facingRight && horizontal < 0)
+        {
+            Flip();
+        }
+        else if (!facingRight && horizontal > 0)
+        {
+            Flip();
+        }
+
+        if (moveInput != Vector2.zero)
+        {
+            anim.SetBool("isRunning", true);
+        }
+        else
+        {
+            anim.SetBool("isRunning", false);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + moveVelocity * Time.fixedDeltaTime);
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 Scaler = transform.localScale;
+        Scaler.x *= -1;
+        transform.localScale = Scaler;
+    }
+}
---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour {
+
+    public float offset;
+
+    public GameObject projectile;
+    public GameObject shotEffect;
+    public GameObject player;
+    public Transform shotPoint;
+    public Animator camAnim;
+
+    private float timeBtwShots;
+    public float startTimeBtwShots;
+
+    private void Update()
+    {
+        Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        float rotZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, 0f, rotZ + offset);
+
+        var rotZBasedOnPlayer = rotZ + offset;
+
+        Debug.Log(rotZ);
+        if (player.transform.localScale.x == -1)
+        {
+            if (rotZ <= 90 && rotZ >= -90)
+            {
+                transform.localScale = new Vector3(-1, 1, 1);
+            }
+            else
+            {
+                transform.localScale = new Vector3(1, 1, 1);
+            }
+        }
+        else
+        {
+            if (rotZ <= 90 && rotZ >= -90)
+            {
+                transform.localScale = new Vector3(1, 1, 1);
+            }
+            else
+            {
+                transform.localScale = new Vector3(-1, 1, 1);
+            }
+        }
+
+
+
+        if (timeBtwShots <= 0)
+        {
+            if (Input.GetMouseButton(0))
+            {
+                Instantiate(shotEffect, shotPoint.position, Quaternion.identity);
+                camAnim.SetTrigger("shake");
+                Instantiate(projectile, shotPoint.position, transform.rotation);
+                timeBtwShots = startTimeBtwShots;
+            }
+        }
+        else {
+            timeBtwShots -= Time.deltaTime;
+        }
+    }
+}
